bbcd7ba7db6076831da812a0ed0a77eb9ed31de6
Andrii Sergiienko <andrey.sergienko@gmail.com>
2015-10-09 22:43:11 +0300

    fix for BERT encoder

--- js/WebRTCO.js
+++ js/WebRTCO.js
@@ -587,42 +587,48 @@ var WebRTCOAdapter = function() {
  */
 var WebRTCO_BERT = function() {
     var API = {};
-    
+
     function utf8_toByteArray(str) {
-    var byteArray = [];
-    if (str !== undefined && str !== null)
-    for (var i = 0; i < str.length; i++)
-        if (str.charCodeAt(i) <= 0x7F) byteArray.push(str.charCodeAt(i));
+        var byteArray = [];
+        if (str !== undefined && str !== null)
+            for (var i = 0; i < str.length; i++)
+                if (str.charCodeAt(i) <= 0x7F) byteArray.push(str.charCodeAt(i));
         else {
             var h = encodeURIComponent(str.charAt(i)).substr(1).split('%');
             for (var j = 0; j < h.length; j++) byteArray.push(parseInt(h[j], 16)); }
-    return {t:107,v:byteArray}; };
+        return {t:107,v:byteArray}; };
 
-    function utf8_dec(t) { var i=c=c1=c2=0,itoa=String.fromCharCode,s=[]; while (i<t.byteLength ) {
-    c=t.getUint8(i); if (c<128) { s+=itoa(c); i++; } else
-    if ((c>191) && (c<224)) { c2=t.getUint8(i+1); s+=itoa(((c&31)<<6)|(c2&63)); i+=2; }
-    else { c2=t.getUint8(i+1); c3=t.getUint8(i+2); s+=itoa(((c&15)<<12)|((c2&63)<<6)|(c3&63));
-        i+=3; } } return s; }
+    function utf8_dec(ab) {
+        if (!(ab instanceof ArrayBuffer)) ab = new Uint8Array(utf8_toByteArray(ab).v).buffer;
+        var t=new DataView(ab),i=c=c1=c2=0,itoa=String.fromCharCode,s=[]; while (i<t.byteLength ) {
+            c=t.getUint8(i); if (c<128) { s+=itoa(c); i++; } else
+            if ((c>191) && (c<224)) { c2=t.getUint8(i+1); s+=itoa(((c&31)<<6)|(c2&63)); i+=2; }
+            else { c2=t.getUint8(i+1); c3=t.getUint8(i+2); s+=itoa(((c&15)<<12)|((c2&63)<<6)|(c3&63));
+                   i+=3; } } return s; }
 
     // BERT Encoder
 
-    function ar(o) { return Array.isArray(o.v)?o.v:utf8_toByteArray(o.v).v;}
-    function fl(a) { return a.reduce(function(f,t){return f.concat(Array.isArray(t)?fl(t):t);},[]);}
+    function uc(u1,u2) { if (u1.byteLength == 0) return u2; if (u2.byteLength == 0) return u1; }
+    function ar(o)     { return o.v instanceof ArrayBuffer ? new Uint8Array(o.v) : o.v instanceof Uint8Array ? o.v :
+                     Array.isArray(o.v) ? new Uint8Array(o.v) : new Uint8Array(utf8_toByteArray(o.v).v);}
+    function fl(a)     { return a.reduce(function(f,t){ return uc(f, t instanceof Uint8Array ? t :
+                     Array.isArray(t) ? fl(t) : new Uint8Array([t]) ); }, new Uint8Array()); }
     API.atom = function(o)   { return {t:100,v:utf8_toByteArray(o).v}; };
-    API.bin = function(o)    { return {t:109,v:utf8_toByteArray(o).v}; };
+    API.bin = function(o)    { return {t:109,v:o instanceof ArrayBuffer ? new Uint8Array(o) : o instanceof Uint8Array ? o : utf8_toByteArray(o).v}; };
     API.tuple = function()   { return {t:104,v:Array.apply(null,arguments)}; };
     API.list = function()    { return {t:108,v:Array.apply(null,arguments)}; };
     API.number = function(o) { return {t:98,v:o}; };
-    function enc(o)    { return new Uint8Array(fl([131,ein(o)])); }
+    function enc(o)    { return fl([131,ein(o)]); }
     function ein(o)    { return Array.isArray(o)?en_108({t:108,v:o}):eval('en_'+o.t)(o); }
     function en_undefined(o) { return [106]; }
     function en_98(o)  { return [98,o.v>>>24,(o.v>>>16)&255,(o.v>>>8)&255,o.v&255]; }
     function en_97(o)  { return [97,o.v];}
     function en_106(o) { return [106];}
-    function en_100(o) { return [100,o.v.length>>>8,o.v.length&255,ar(o)]; }
+    function en_100(o) { return [100,o.v.length>>>8,o.v.length&255,ar(o)];}
     function en_107(o) { return [107,o.v.length>>>8,o.v.length&255,ar(o)];}
     function en_104(o) { var l=o.v.length,r=[]; for(var i=0;i<l;i++)r[i]=ein(o.v[i]); return [104,l,r]; }
-    function en_109(o) { var l=o.v.length;return[109,l>>>24,(l>>>16)&255,(l>>>8)&255,l&255,ar(o)]; }
+    function en_109(o) { var l=o.v instanceof ArrayBuffer ? o.v.byteLength : o.v.length;
+                     return[109,l>>>24,(l>>>16)&255,(l>>>8)&255,l&255,ar(o)]; }
     function en_108(o) { var l=o.v.length,r=[]; for(var i=0;i<l;i++)r.push(ein(o.v[i]));
                      return o.v.length==0?[106]:[108,l>>>24,(l>>>16)&255,(l>>>8)&255,l&255,r,106]; }
 
@@ -630,16 +636,16 @@ var WebRTCO_BERT = function() {
 
     function nop(b) { return []; };
     function int(b) { return b==1?sx.getUint8(ix++):sx.getInt32((a=ix,ix+=4,a)); };
-    function dec(d) { sx=new DataView(d);ix=0; if(sx.getUint8(ix++)!==131)throw("BERT?"); return din();};
+    function dec(d) { sx=new DataView(d);ix=0; if(sx.getUint8(ix++)!==131)throw("BERT?"); return din(); };
     function str(b) { var dv,sz=(b==2?sx.getUint16(ix):sx.getInt32(ix));ix+=b;
-                  return utf8_dec(new DataView(sx.buffer.slice(ix,ix+=sz))); };
+                      var r=sx.buffer.slice(ix,ix+=sz); return b==2?utf8_dec(r):r; };
     function run(b) { var sz=(b==1?sx.getUint8(ix):sx.getUint32(ix)),r=[]; ix+=b;
-                  for(var i=0;i<sz;i++) r.push(din()); if(b==4)ix++; return r; };
+                      for(var i=0;i<sz;i++) r.push(din()); if(b==4)ix++; return r; };
     function din()  { var c=sx.getUint8(ix++),x; switch(c) { case 97: x=[int,1];break;
-                  case 98:  x=[int,4]; break; case 100: x=[str,2]; break;
-                  case 104: x=[run,1]; break; case 107: x=[str,2]; break;
-                  case 108: x=[run,4]; break; case 109: x=[str,4]; break;
-                  default:  x=[nop,0]; } return {t:c,v:x[0](x[1])};};
+                      case 98:  x=[int,4]; break; case 100: x=[str,2]; break;
+                      case 104: x=[run,1]; break; case 107: x=[str,2]; break;
+                      case 108: x=[run,4]; break; case 109: x=[str,4]; break;
+                      default:  x=[nop,0]; } return {t:c,v:x[0](x[1])};};
 
     API.encode = function(t) {
         return enc(t);
